cdad28ca01c81c369a1358bc09fa1f5e6656dad8
bchaudhary <belalc80@gmail.com>
2017-06-03 07:26:41 +0100

    updated functions

--- text2vec.py
+++ text2vec.py
@@ -1,32 +1,35 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-
 import numpy as np
 
 
 def make_feature_vec(words, model, num_features):
-    # Function to average all of the word vectors in a given
-    # paragraph
-    #
+    """
+    Function to average all of the word vectors in a given paragraph
+    :param words:
+    :param model:
+    :param num_features:
+    :return:
+    """
     # Pre-initialize an empty numpy array (for speed)
     feature_vec = np.zeros((num_features,), dtype="float32")
-    #
-    nwords = 0
-    #
+
+    n_words = 0
+
     # Index2word is a list that contains the names of the words in
     # the model's vocabulary. Convert it to a set, for speed
     index2word_set = set(model.index2word)
-    #
+
     # Loop over each word in the review and, if it is in the model's
     # vocabulary, add its feature vector to the total
     for word in words:
         if word in index2word_set:
-            nwords += 1
+            n_words += 1
             feature_vec = np.add(feature_vec, model[word])
-    #
+
     # Divide the result by the number of words to get the average
-    feature_vec = np.divide(feature_vec, nwords)
+    # feature_vec = np.divide(feature_vec, n_words)
     return feature_vec
 
 
@@ -37,17 +40,17 @@ def get_avg_feature_vecs(reviews, model, num_features):
     # Initialize a counter
     counter = 0
     #
-    # Preallocate a 2D numpy array, for speed
+    # Pre-allocate a 2D numpy array, for speed
     review_feature_vecs = np.zeros((len(reviews), num_features), dtype="float32")
     #
     # Loop through the reviews
     for review in reviews:
-        # Print a status message every 1000th review
-        if counter % 1000 == 0:
-            print("Review %d of %d" % (counter, len(reviews)))
+        # Print a status message
+        # if counter % 1000 == 0:
+        # print("Document %d of %d" % (counter, len(reviews)))
 
         # Call the function (defined above) that makes average feature vectors
-        review_feature_vecs[counter] = make_feature_vec(review, model,num_features)
+        review_feature_vecs[counter] = make_feature_vec(review, model, num_features)
         # Increment the counter
         counter += 1
     return review_feature_vecs
