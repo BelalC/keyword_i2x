d0804f83b6cf7ebc40cc53890ede76584242be7d
bchaudhary <belalc80@gmail.com>
2017-06-04 01:09:18 +0100

    included Word2Vec model options

--- keyword_xtract.py
+++ keyword_xtract.py
@@ -4,7 +4,7 @@
 import gensim
 import argparse
 import numpy as np
-
+import wget
 from os import path
 from glob import glob
 from itertools import compress
@@ -33,8 +33,9 @@ ap.add_argument("-w", "--word_len", type=int, default=4,
                 help="maximum number of words in key word")
 ap.add_argument("-f", "--word_freq", type=int, default=3,
                 help="minimum threshold number of occurrences for key word")
-ap.add_argument("-m", "--model", type=str, default="w2v_models/GoogleNews-vectors-negative300.bin.gz",
-                help="path to user-defined Word2Vec model, default is trained on Google News dataset")
+ap.add_argument("-m", "--model", type=str, default="w2v_models/mini_model.bin.gz",
+                help="choice of model or path to user-defined Word2Vec model, default model is a truncated "
+                     "version of model trained on Google News dataset")
 ap.add_argument("-t", "--test", type=str, default="evaluation/",
                 help="path to directory containing test text files used to evaluate relevance of extracted key words")
 ap.add_argument("-o", "--output", type=str, default="results/results.txt",
@@ -49,9 +50,52 @@ required_ap.add_argument("-i", "--input",
                          help="path to a single document to extract keywords from")
 args = vars(ap.parse_args())
 
+
+# ====== DOWNLOADING Word2Vec MODELS ======
+# =========================================
+
+# Map model names to download links
+MODELS = {
+    # "google": "https://drive.google.com/file/d/0B7XkCwpI5KDYNlNUTTlSS21pQmM/edit",
+    "glove_6B": "http://nlp.stanford.edu/data/glove.6B.300d.zip",
+    "glove_42B": "http://nlp.stanford.edu/data/glove.42B.300d.zip",
+    "glove_840B": "http://nlp.stanford.edu/data/glove.840B.300d.zip",
+    "glove_twitter": "http://nlp.stanford.edu/data/glove.twitter.27B.zip"
+}
+
+if args["model"] not in MODELS:
+    if not path.exists(args["model"]):
+        raise AssertionError("The --model command line argument should be a key in the `MODELS` dictionary, "
+                             "or else should be a path to a custom Word2Vec model")
+
+
+# Download word embeddings for Word2Vec model, if not already present
+model_dir = "w2v/" + args["model"] + ".bin"
+if args["model"] in MODELS:
+    if not path.exists(model_dir):
+        print("downloading ", args["model"], " Word2Vec model")
+        print("this may take a while, file may be up to 2GB in size")
+        url = MODELS[args["model"]]
+        file = wget.download(url=url, out=model_dir)
+        print("downloaded model!")
+
+# load Word2Vec model
+if args["model"] in MODELS:
+    if path.exists(model_dir):
+        print("loading ", args["model"], " Word2Vec model")
+        print("this may take a few minutes...")
+        model = gensim.models.KeyedVectors.load_word2vec_format(model_dir, binary=True)
+        print("loaded model!")
+else:
+    print("loading Word2Vec model")
+    print("this may take a few minutes...")
+    model = gensim.models.KeyedVectors.load_word2vec_format(args["model"], binary=True)
+    print("loaded model!")
+
 # ====== KEYWORD EXTRACTION ======
 # ================================
 
+print("extracting keywords")
 # Initialize RAKE object,
 rake_object = Rake(stop_words_path=args["stopword"], min_char_length=args["char"],
                    max_words_length=args["word_len"], min_keyword_frequency=args["word_freq"])
@@ -66,14 +110,8 @@ print("%d keywords extracted" % (len(keywords)))
 # 3. print results
 # print("Keywords:", keywords)
 
-# =========== Word2Vec ===========
-# ================================
-
-# load Word2Vec trained on Google corpus
-print("loading Word2Vec model")
-print("this may take a few minutes...")
-model = gensim.models.KeyedVectors.load_word2vec_format(args["model"], binary=True, limit=600000)
-print("loaded model!")
+# =========== Word2Vec - KEYWORDS ===========
+# ===========================================
 
 # filter previously generated keywords to include only those present in the Word2Vec model's vocabulary
 # this is done automatically for the test documents
@@ -85,7 +123,7 @@ keyword_in_model = list(compress(keywords, bool_split))
 sorted_keyword = sorted(keyword_in_model, key=lambda x: x[1], reverse=True)
 n_keywords = int(args["n_keyword"]*len(sorted_keyword)//100)
 keyword_list = sorted_keyword[0:n_keywords]
-print("using top %d keywords " % (n_keywords))
+print("using top %d keywords " % n_keywords)
 
 # compute vector representations of extracted keywords
 keyword_vecs = [(model.word_vec(word[0])) for word in keyword_list]
@@ -95,13 +133,13 @@ keyword_vecs = [(model.word_vec(word[0])) for word in keyword_list]
 #                                         min_char_length=2, max_words_length=2)
 
 
-# ======= EVALUATION DOCS ========
-# ================================
+# ========== Word2Vec - TEST DOCS ===========
+# ===========================================
 
 # extract average Word2Vec representation for test documents, to be utilised for evaluating keywords
 test_dirs = glob(path.join(args["test"], "*txt"))
 test_docs = [doc.read() for doc in [open(test_file, "r") for test_file in test_dirs]]
-print("extracting vector representation of evaluation docs; %d docs in total" % (len(test_docs)))
+print("extracting vector representation of test docs; %d docs in total" % (len(test_docs)))
 test_vecs = [get_avg_feature_vecs([doc],
                                   model=model,
                                   num_features=model.vector_size)
@@ -123,7 +161,6 @@ for vec in test_vecs:
 sum_keyword = np.zeros_like(similar_keywords[0])
 for doc in similar_keywords:
     sum_keyword = sum_keyword + doc
-
 keyword_similarities = sum_keyword/3
 
 # list of keywords
