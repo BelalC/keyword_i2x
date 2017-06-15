7a5baeee2595241e84737438ddcb03c9cf800a1d
bchaudhary <belalc80@gmail.com>
2017-06-16 00:38:01 +0100

    requirements

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from distutils.core import setup
 
 setup(
   name='keyword_xtract',
-  packages=['keyword_xtract'], # this must be the same as the name above
+  packages=['keyword_xtract'],
   version='0.1',
   description='Keyword extraction with RAKE, and keyword ranking using vector representations - created as part'
               'of a toy challenge',
-  author = 'Belal Chaudhary',
-  url = 'https://github.com/BelalC/keyword_i2x', # use the URL to the github repo
-  download_url = 'https://github.com/peterldowns/mypackage/archive/0.1.tar.gz',
-  keywords = ['keyword', 'key phrase','RAKE', 'word2vec'], # arbitrary keywords
-  classifiers = [],
+  author='Belal Chaudhary',
+  author_email="belalc80@gmail.com",
+  url='https://github.com/BelalC/keyword_i2x',
+  download_url='https://github.com/peterldowns/mypackage/archive/0.1.tar.gz',
+  keywords=['keyword', 'key phrase','RAKE', 'word2vec'],
+  classifiers=[],
 )
