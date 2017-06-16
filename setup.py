63a208149cac6cf4cf8cdf5433a01ca661519bc5
bchaudhary <belalc80@gmail.com>
2017-06-16 02:29:12 +0100

    restructuring

--- setup.py
+++ setup.py
@@ -10,6 +10,6 @@ setup(
   author_email="belalc80@gmail.com",
   url='https://github.com/BelalC/keyword_i2x',
   download_url='https://github.com/peterldowns/mypackage/archive/0.1.tar.gz',
-  keywords=['keyword', 'key phrase','RAKE', 'word2vec'],
+  keywords=['keyword', 'key phrase', 'RAKE', 'word2vec'],
   classifiers=[],
 )
