c7a691dae7910a301d7fcec0491f0827e3497c04
bchaudhary <belalc80@gmail.com>
2017-06-16 16:36:00 +0100

    updated for PyPi

--- setup.py
+++ setup.py
@@ -1,15 +1,23 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
   name='keyword_xtract',
-  packages=['keyword_xtract'],
-  version='0.1',
+  packages=find_packages(where="src"),
+  package_dir={"": "src"},
+  version='0.1.1',
   description='Keyword extraction with RAKE, and keyword ranking using vector representations - created as part'
               'of a toy challenge',
   author='Belal Chaudhary',
   author_email="belalc80@gmail.com",
   url='https://github.com/BelalC/keyword_i2x',
-  download_url='https://github.com/peterldowns/mypackage/archive/0.1.tar.gz',
   keywords=['keyword', 'key phrase', 'RAKE', 'word2vec'],
-  classifiers=[],
+  classifiers=[
+      'Development Status :: 3 - Alpha',
+      'Intended Audience :: Developers',
+      "Natural Language :: English",
+      "License :: OSI Approved :: MIT License",
+      "Operating System :: OS Independent",
+      "Programming Language :: Python",
+      "Programming Language :: Python :: 2.7",
+      "Programming Language :: Python :: 3.5"]
 )
